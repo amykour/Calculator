15afede162b25e157fb9df2693ec946ef330b8a0
Alena <amy.kour@yandex.ru>
2024-06-17 00:06:23 +0300

    iniitial

---
+++ History/HistoryTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  HistoryTableViewCell.swift
+//  TinkoffCalculator
+//
+//  Created by Алена on 10.03.2024.
+//
+
+import Foundation
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+    
+    @IBOutlet private weak var expressionLabel: UILabel!
+    @IBOutlet private weak var resultLabel: UILabel!
+    
+    func configure(with expression: String, result: String) {
+        expressionLabel.text = expression
+        resultLabel.text = result
+    }
+    
+}
---
+++ TinkoffCalculator/ViewController.swift
@@ -0,0 +1,171 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Алена on 01.03.2024.
+//
+
+import UIKit
+import Foundation
+
+enum Operation: String {
+    case add = "+"
+    case substract = "-"
+    case multiply = "X"
+    case divide = "/"
+    
+    func calculate(_ number1: Double, _ number2: Double) throws -> Double {
+        switch self {
+        case .add:
+            return number1 + number2
+            
+        case .substract:
+            return number1 - number2
+            
+        case .multiply:
+            return number1 * number2
+            
+        case .divide:
+            if number2 == 0 {
+                throw CalculationError.dividedByZero
+            }
+            return number1 / number2
+        }
+    }
+}
+
+enum CalculationError: Error {
+    case dividedByZero
+}
+
+enum CalculationHistoryItem {
+    case number(Double)
+    case operation(Operation)
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var historyButton: UIButton!
+    
+    lazy var numberFormatter: NumberFormatter = {
+       let numberFormatter = NumberFormatter()
+        
+        numberFormatter.usesGroupingSeparator = false
+        numberFormatter.locale = Locale(identifier: "ru_RU")
+        numberFormatter.numberStyle = .decimal
+        
+        return numberFormatter
+    }()
+    
+    var calculationHistory: [CalculationHistoryItem] = []
+    var calculations: [Calculation] = []
+    let calculationHistoryStorage = CalculationHistoryStorage()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resetLabelText()
+        
+        historyButton.accessibilityIdentifier = "historyButton"
+        calculations = calculationHistoryStorage.loadHistory()
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.titleLabel?.text else { return }
+        
+        if buttonText == "," && label.text?.contains(",") == true {
+            return
+        }
+        
+        if label.text == "0" && buttonText != "," {
+            label.text = buttonText
+        } else {
+            label.text?.append(buttonText)
+        }
+    }
+    
+    @IBAction func operationButtonPressed(_ sender: UIButton) {
+        guard
+            let buttonText = sender.titleLabel?.text,
+            let buttonOperation = Operation(rawValue: buttonText)
+            else { return }
+        
+        guard
+            let labelText = label.text,
+            let labelNumber = numberFormatter.number(from: labelText)?.doubleValue
+            else { return }
+        
+        calculationHistory.append(.number(labelNumber))
+        calculationHistory.append(.operation(buttonOperation))
+        
+        resetLabelText()
+    }
+    
+    @IBAction func cleanButtonPressed(_ sender: UIButton) {
+        calculationHistory.removeAll()
+        
+        resetLabelText()
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        guard
+            let labelText = label.text,
+            let labelNumber = numberFormatter.number(from: labelText)?.doubleValue
+            else { return }
+        
+        calculationHistory.append(.number(labelNumber))
+        
+        do {
+            let result = try calculate()
+            
+            label.text = numberFormatter.string(from: NSNumber(value: result))
+            let newCalculation = Calculation(expression: calculationHistory, result: result, date: Date())
+            calculations.append(newCalculation)
+            calculationHistoryStorage.setHistory(calculation: calculations)
+            
+        } catch {
+            label.text = "Ошибка"
+        }
+        
+        calculationHistory.removeAll()
+    }
+    
+    func calculate() throws -> Double {
+        guard case .number(let firstNumber) = calculationHistory[0] else { return 0 }
+        
+        var currentResult = firstNumber
+        
+        for index in stride(from: 1, to: calculationHistory.count - 1, by: 2) {
+            guard
+                case .operation(let operation) = calculationHistory[index],
+                case .number(let number) = calculationHistory[index + 1]
+                else { break }
+            
+            currentResult = try operation.calculate(currentResult, number)
+        }
+        
+        return currentResult
+    }
+    
+    @IBAction func showCalculationsList(_ sender: UIButton) {
+        
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let calculationsListVC = sb.instantiateViewController(identifier: "CalculationsListViewController")
+        if let vc = calculationsListVC as? CalculationsListViewController {
+            vc.title = "History"
+            vc.calculations = calculations
+        }
+        
+        navigationController?.pushViewController(calculationsListVC, animated: true )
+        
+    }
+   
+    func resetLabelText() {
+        label.text = "0"
+    }
+}
